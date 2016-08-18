5d8195bc319cb7717e841414d54507a932b617bf
jonathan.rankin <jonathan.rankin@guardian.co.uk>
2016-08-18 15:33:56 +0100

    first commit

---
+++ src/js/embed.js
@@ -0,0 +1,46 @@
+import iframeMessenger from 'guardian/iframe-messenger'
+import reqwest from 'reqwest'
+import embedHTML from './text/embed.html!text'
+import * as helper from 'src/utils/helper'
+import $ from '$'
+
+
+const CUSTOM_AMOUNT = document.querySelector('.js-amount-field');
+const ACTIVE_CLASS = 'active';
+const AMOUNT_CLASS = 'js-amount';
+const $AMOUNT_PICKER = $('[data-amount]');
+const $SUMBIT = document.querySelector('.js-submit-input');
+
+
+window.init = function init() {
+    iframeMessenger.enableAutoResize();
+    console.log("start init");
+    console.log($AMOUNT_PICKER);
+    $AMOUNT_PICKER.each(el => el.addEventListener('click', ev => {
+        console.log(el, ev);
+        let element = ev.currentTarget;
+        let amount = element.getAttribute('data-amount');
+        select(element);
+
+       setAmount(amount);
+
+    }));
+
+    $SUMBIT.addEventListener('click', ev => {
+        console.log("submit");
+        console.log(ev);
+        window.location="https://contribute.theguardian.com?amount=" + document.querySelector('input.' + AMOUNT_CLASS).value;
+    });
+};
+
+function select(el) {
+    $(helper.getSpecifiedParent(el, 'js-button-group').querySelectorAll('.js-button')).removeClass(ACTIVE_CLASS);
+    $(el).addClass(ACTIVE_CLASS);
+}
+
+function setAmount(amount) {
+    $('input.' + AMOUNT_CLASS).val(amount);
+    }
+
+
+init();
